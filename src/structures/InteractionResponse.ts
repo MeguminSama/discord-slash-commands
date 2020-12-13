d7cd199e2d2e3a1949d3b83d368234e121563e5c
A5rocks <40616000+A5rocks@users.noreply.github.com>
2020-12-13 12:14:20 +0900

    Fix typings
    This should really be a MAJOR version bump, however that sounds a little
    extreme.

--- src/structures/InteractionResponse.ts
+++ src/structures/InteractionResponse.ts
@@ -1,13 +1,7 @@
 import { InteractionApplicationCommandCallbackData } from "./InteractionApplicationCommandCallbackData";
+import { InteractionResponseType } from "./InteractionResponseType";
 
 export type InteractionResponse = {
   type: InteractionResponseType;
   data?: InteractionApplicationCommandCallbackData;
 };
-export enum InteractionResponseType {
-  PONG = 1,
-  ACKNOWLEDGE = 2,
-  CHANNEL_MESSAGE = 3,
-  CHANNEL_MESSAGE_WITH_SOURCE = 4,
-  ACK_WITH_SOURCE = 5,
-}
